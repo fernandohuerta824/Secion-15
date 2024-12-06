4e5d6f00047c0f95f3344f7e8bd34ac411ede431
Fernando Huerta <fernando.huerta4313@alumnos.udg.mx>
2024-12-05 20:43:36 -0600

    A ver si ya jala

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const fileFilter = (req, file, cb) => {
 }
 
 // const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 // app.use(helmet({strictTransportSecurity: false}))
 app.use(compression())
